1a653f4860a953d97f7ffc7a169807806986aab1
tariq <tariqabdelghani@gmail.com>
2019-11-30 22:13:12 +0200

    adding nessary packages to make basic application structure, making user model, and start up files , configuration files.

---
+++ startup/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//const errorHandler = require('../middleware/error-middleware');
+//const notfound = require('../middleware/notfound-middleware');
+
+
+
+module.exports = function (app){
+
+    //app.use('/', hanlder);
+    app.use(express.json());
+    app.get('/',(req, res, next)=>{
+        res.send('<h1> Hello World!</h1>');
+    });
+
+    //app.use(notfound);
+    //app.use(errorHandler);
+}
