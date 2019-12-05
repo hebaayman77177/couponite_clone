72e40f4fffc345edd22a06dfec9997c4013f3fb5
hebaayman77177 <hebaayman77177@gmail.com>
2019-12-05 10:47:40 +0200

    merged

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,18 +1,11 @@
 const router = require("express").Router();
 const passport = require("passport");
 const userController = require("../controllers/user-controller");
-<<<<<<< HEAD
 const authanticate = require("../middlewares/autahnitcate-middleware");
 const { User } = require("../models/user");
-||||||| eb01060
-const authanticate = require("../middlewares/autahnitcate-middleware");
-
-const passport = require("passport");
-=======
 const googleUtils = require('../utils/login-with-google');
 
 const passport = require("passport");
->>>>>>> 42ebddc03887338c058f77f9a96faa1ea0194f7f
 
 router.get("/auth/facebook", passport.authenticate("facebook"));
 
@@ -33,15 +26,9 @@ router.post("/login", userController.login);
 router.post("/confirmToken", userController.confirmToken);
 router.post("/resendToken", userController.resendToken);
 router.post("/forgotPassword", userController.forgotPassword);
-<<<<<<< HEAD
 router.get("/resetPassword/:token", userController.resetPassword);
 router.post("/changePhone", authanticate, userController.changePhone);
 router.post("/resetPhone", authanticate, userController.resetPhone);
-||||||| eb01060
-router.post("/resetPassword", userController.resetPassword);
-router.post("/changePhone", authanticate, userController.changePhone);
-router.post("/resetPhone", authanticate, userController.resetPhone);
-=======
 router.post("/resetPassword", userController.resetPassword);
 router.post("/changePhone", userController.changePhone);
 router.post("/resetPhone", userController.resetPhone);
@@ -53,5 +40,4 @@ touter.put("/myPassword", userController.changeMyPasword);
 
 router.get('/auth/google', googleUtils.authenticateWithGoogle);
 router.get('/auth/google/callback', googleUtils.googleCallBack);
->>>>>>> 42ebddc03887338c058f77f9a96faa1ea0194f7f
 module.exports = router;
